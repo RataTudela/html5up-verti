06a27ac39a7cb3fb9d66e2575b9c28e0cc474e81
RataTudela <ben.olavarria@duocuc.cl>
2025-09-04 21:24:10 -0400

    Terminados las validacions de los formurlarios de ingreso de datos en el registro de usuarios, todo listo por esa parte.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,28 +4,151 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 // Funciones
-	function correoYcontraseña() {
-          event.preventDefault();
-            const email = document.getElementById('email').value;
-            const password = document.getElementById('contraseña').value;
+function correoYcontraseña() {
+    event.preventDefault(); 
+    
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('contraseña').value;
+    
+    var termina = ["@duocuc.cl","@profesor.duoc.cl","@gmail.com"];
+    var verificarCorreo = false;
 
-            if (!email || email.length > 100) {
-                document.getElementById('emailError').innerText = 'Correo No ingresado o demasiado largo.';
-                return;
-            }else{
-				document.getElementById('emailError').innerText = '';
-			}
-         
-            if (!password || password.length < 4 || password.length > 10) {
-                document.getElementById('contraseñaError').innerText = 'Contraseña No ingresada o inválida.';
-                return;
-            }else{
-				document.getElementById('contraseñaError').innerText= '';
-			}
-			
-            console.log('Correo:', email);
-            console.log('Contraseña:', password);
+    for (var i = 0; i < termina.length; i++) {
+        if (email.endsWith(termina[i])) {
+            correoValido = true;
+            break;
         }
+    }
+    
+    if (!email) {
+        document.getElementById('emailError').innerText = 'Correo no ingresado.';
+        return;
+    } else if (email.length > 100) {
+        document.getElementById('emailError').innerText = 'Correo demasiado largo.';
+        return;
+    } else if (!correoValido) {
+        document.getElementById('emailError').innerText = 'Correo no termina en "@duocuc.cl","@profesor.duoc.cl","@gmail.com';
+        return;
+    } else {
+        document.getElementById('emailError').innerText = '';
+    }
+    
+    if (!password) {
+        document.getElementById('contraseñaError').innerText = 'Contraseña no ingresada.';
+        return;
+    } else if (password.length < 4 || password.length > 10) {
+        document.getElementById('contraseñaError').innerText = 'Contraseña inválida.';
+        return;
+    } else {
+        document.getElementById('contraseñaError').innerText = '';
+    }
+    
+    console.log('Correo:', email);
+    console.log('Contraseña:', password);
+}
+
+function correoYcontraseñaRegistro() {
+    event.preventDefault(); 
+    
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('contraseña').value;
+	var Nombre = document.getElementById('nombre').value;
+    var validarContra = document.getElementById('confirmarContraseña').value;
+	var telefono = document.getElementById('Telefono').value;
+	var region = document.getElementById('Region').value;
+	var comuna = document.getElementById('Comuna').value;
+
+    var termina = ["@duocuc.cl","@profesor.duoc.cl","@gmail.com"];
+    var verificarCorreo = false;
+
+    for (var i = 0; i < termina.length; i++) {
+        if (email.endsWith(termina[i])) {
+            verificarCorreo = true;
+            break;
+        }
+    }
+
+    if (!email) {
+        document.getElementById('emailError').innerText = 'Correo no ingresado';
+        return;
+    } else if (email.length > 100) {
+        document.getElementById('emailError').innerText = 'Correo demasiado largo';
+        return;
+    } else if (!verificarCorreo) {
+        document.getElementById('emailError').innerText = 'Correo no termina en "@duocuc.cl","@profesor.duoc.cl","@gmail.com';
+        return;
+    } else {
+        document.getElementById('emailError').innerText = '';
+    }
+    
+    if (!password) {
+        document.getElementById('contraseñaError').innerText = 'Contraseña no ingresada';
+        return;
+    } else if (password.length < 4 || password.length > 10) {
+        document.getElementById('contraseñaError').innerText = 'Contraseña mas corta que 4 o mas larga que 10';
+        return;
+    } else {
+        document.getElementById('contraseñaError').innerText = '';
+    }
+
+	if (!Nombre) {
+        document.getElementById('nombreError').innerText = 'Nombre no ingresado';
+        return;
+    } else if (Nombre.length > 50 || Nombre.length < 3) {
+        document.getElementById('nombreError').innerText = 'Nombre demasiado corto o largo';
+        return;
+    } else {
+        document.getElementById('nombreError').innerText = '';
+    }
+    
+	if (!validarContra){
+		document.getElementById('confirmarContraseñaError').innerText = 'No Confirmo su contraseña';
+        return;
+	} else if (validarContra !== password){
+		document.getElementById('confirmarContraseñaError').innerText = 'Contraseñas no coinciden';
+		return;
+	} else {
+        document.getElementById('confirmarContraseñaError').innerText = '';
+    }
+
+	if(!telefono){
+		document.getElementById('telefonoError').innerText = 'No Ingreso su telefono';
+		return;
+	} else if(telefono.length != 9){
+		document.getElementById('telefonoError').innerText = 'Telefono debe tener 9 digitos';
+		return;
+	} else {
+		document.getElementById('telefonoError').innerText = '';
+	}
+
+	if(!region){
+		document.getElementById('regionError').innerText = 'No Ingreso su region';
+		return;
+	} else if(region.length < 4 || region.length > 20){
+		document.getElementById('regionError').innerText = 'Region demasiado corta o larga';
+		return;
+	} else {
+		document.getElementById('regionError').innerText = '';
+	}
+
+	if(!comuna){
+		document.getElementById('comunaError').innerText = 'No Ingreso su comuna';
+		return;
+	} else if(comuna.length < 4 || comuna.length > 20){
+		document.getElementById('comunaError').innerText = 'Comuna demasiado corta o larga';
+		return;
+	} else {
+		document.getElementById('comunaError').innerText = '';
+	}
+
+    console.log('Correo:', email);
+    console.log('Contraseña:', password);
+	console.log('Nombre:', Nombre);
+	console.log('ContraseñaConfirmada:', validarContra);
+	console.log('Telefono:', telefono);
+	console.log('Region:', region);
+	console.log('Comuna:', comuna);
+}
 // Fin Funciones
 (function($) {
 
